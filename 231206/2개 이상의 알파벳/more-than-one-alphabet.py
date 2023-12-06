996f72f5c7653c89d0cff1015611343875864d44
SongAhyeon01 <114563915+SongAhyeon01@users.noreply.github.com>
2023-12-06 17:41:24 +0900

    Title: 2개 이상의 알파벳, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231206/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 231206/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -2,11 +2,11 @@ def cal(A):
     dic = {}
     for i in range(len(A)-1):
         if A[i] in dic:
+            print(A[i])
             dic[i] += 1
         else:
             dic[i] = 1
     if len(dic) > 3:
-        print(len(dic))
         return print("Yes")
     else:
         return print("No")
