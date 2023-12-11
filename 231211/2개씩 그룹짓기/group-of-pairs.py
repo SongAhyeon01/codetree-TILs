302b3a7fce9730da8dda2b65c97a9ae3aea07d59
SongAhyeon01 <114563915+SongAhyeon01@users.noreply.github.com>
2023-12-11 15:16:04 +0900

    Title: 2개씩 그룹짓기, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231211/2개씩 그룹짓기/group-of-pairs.py
+++ 231211/2개씩 그룹짓기/group-of-pairs.py
@@ -4,10 +4,11 @@ a = list(map(int, input().split()))
 aa = sorted(a)
 bb = list(reversed(aa))
 
+
 for i in range(n):
     max = 0
     now = aa[i] + bb[i]
     if now > max:
-        max, now = now, max
+        max = now
 
 print(max)
