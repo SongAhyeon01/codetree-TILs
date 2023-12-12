45299d82dd24f0ced0a510b0b7ab2aaaef080739
SongAhyeon01 <114563915+SongAhyeon01@users.noreply.github.com>
2023-12-12 14:27:59 +0900

    Title: DateTime to DateTime, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231212/DateTime to DateTime/datetime-to-datetime.py
+++ 231212/DateTime to DateTime/datetime-to-datetime.py
@@ -1,10 +1,12 @@
 a, b, c = map(int, input().split())
 
-if a < 11 and b <= 11 and c <= 11:
+
+
+
+mm = a*24*60 + b*60 + c
+ss = 11*24*60 + 11*60 + 11
+
+if mm<ss:
     print(-1)
-
-
 else:
-    mm = a*24*60 + b*60 + c
-    ss = 11*24*60 + 11*60 + 11
     print(mm-ss)
