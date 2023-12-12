a351c52fdb341affe532ab9cb9fc486a81f96dd8
SongAhyeon01 <114563915+SongAhyeon01@users.noreply.github.com>
2023-12-12 14:16:16 +0900

    Title: Date to Date, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 231212/Date to Date/date-to-date.py
+++ 231212/Date to Date/date-to-date.py
@@ -3,17 +3,14 @@ m1, d1, m2, d2 = map(int, input().split())
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 k1 = 0
-for i in range(m1+1):
+for i in range(0, m1):
     k1 += num_of_days[i];
 k1 += d1
 
 k2 = 0
-for i in range(m2+1):
+for i in range(0, m2):
     k2 += num_of_days[i];
 k2 += d2
 
 
-if k2 == k1:
-    print(1)
-else:
-    print(k2-k1)
+print(k2-k1+1)
