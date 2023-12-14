2d2e77a3948c7d10d12637214fc43214f2f18ce1
SongAhyeon01 <114563915+SongAhyeon01@users.noreply.github.com>
2023-12-14 10:20:33 +0900

    Title: 왔다 갔던 구역 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231214/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 231214/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -2,19 +2,19 @@ n = int(input())
 
 line = [0 for i in range(100)]
 
+now = 50
 for _ in range(n):
-    now = 50
     x, r = map(str, input().split())
     x = int(x)
 
     if r == 'R':
-        for k in range(now, x, 1):
+        for k in range(now, now+x, 1):
             line[k] += 1
-            now = now + x
+        now = now + x
     else:
         for k in range(now, now-x, -1):
             line[k] += 1
-            now = now - x
+        now = now - x
 
 
 cnt = 0
