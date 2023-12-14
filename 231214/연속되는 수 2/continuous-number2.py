9fffac70ee1ddfbb0e3630ab30f10f3653a35e33
SongAhyeon01 <114563915+SongAhyeon01@users.noreply.github.com>
2023-12-14 14:00:51 +0900

    Title: 연속되는 수 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231214/연속되는 수 2/continuous-number2.py
+++ 231214/연속되는 수 2/continuous-number2.py
@@ -1,13 +1,21 @@
-n = int(input())
+# 리스트에 저장
+# 리스트를 돌면서 일단 카운트를 1한다
+# 근데 != 뒤에꺼일 경우에 카운트를 초기화한다
+# 만약 카운트가 4인데 초기화됐다?
+# 그럼 안됨
+# 그래서 최대 카운트 수를 저장해야한다
+# != 를 만나면 현재 카운트를 최대 카운트와 비교하고 최대보다 크면 바꾼다
 
+n = int(input())
 list = [int(input()) for _ in range(n)]
 
+max_cnt = 1
 cnt = 1
-for i in range(len(list)):
+for i in range(n):
     if i == 0 or list[i] != list[i-1]:
-        cnt += 0
+        if cnt >= max_cnt:
+            max_cnt = cnt
+            cnt = 1
     else:
         cnt += 1
-    
-
-print(cnt)
+print(max_cnt)
