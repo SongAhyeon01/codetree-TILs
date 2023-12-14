e0ccbd3344f5438cce7c1efac5faa66a1a1c97d7
SongAhyeon01 <114563915+SongAhyeon01@users.noreply.github.com>
2023-12-14 15:22:40 +0900

    Title: 벌금은 누구에게, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231214/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,11 @@
+n, m, k = map(int, input().split())
+
+s_list = [0 for i in range(n+1)]
+
+for i in range(m):
+    s = int(input())
+    s_list[s] += 1
+
+    if s_list[s] >= k:
+        print(s)
+        break
