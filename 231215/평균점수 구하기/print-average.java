4d14f82c77a09448df692c8c42b6b5e1950e7371
SongAhyeon01 <114563915+SongAhyeon01@users.noreply.github.com>
2023-12-15 13:33:20 +0900

    Title: 평균점수 구하기, Time: 151ms, Memory: 11MB, Status: Passed - CodeTree

--- 231215/평균점수 구하기/print-average.java
+++ 231215/평균점수 구하기/print-average.java
@@ -5,15 +5,19 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        double arr[] = new double[8];
+        float arr[] = new float[8];
 
         for (int i=0; i<8; i++) {
-            double s = sc.nextDouble();
+            float s = sc.nextFloat();
             arr[i] = s;
         }
 
-        double sum = Arrays.stream(arr).sum();
-        double avg = sum / 8;
+        float sum = 0;
+        for (int i=0; i<8; i++) {
+            sum += arr[i];
+        }
+        
+        float avg = sum / 8;
         System.out.print(String.format("%.1f", avg));
 
     }
