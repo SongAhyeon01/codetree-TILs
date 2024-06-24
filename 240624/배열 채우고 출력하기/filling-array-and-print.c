267b6c2f4418b1d6a75be3f97d753cedb9c4e6bf
Ahyeon Song <114563915+SongAhyeon01@users.noreply.github.com>
2024-06-24 15:59:46 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240624/배열 채우고 출력하기/filling-array-and-print.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[10];
+
+    for (int i=0; i<10; i++){
+        scanf("%c ", &arr[i]);
+    }
+
+    for (int j=9; j >= 0; j--){
+        printf("%c", arr[j]);
+    }
+
+    return 0;
+}
